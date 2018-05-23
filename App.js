092841a74cac2ae9e93fcd35fbfcb8f13ae9a937
fumdiali <fumdiali76@gmail.com>
2018-05-23 17:14:42 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Main from './app/components/Main';
+
+//type Props = {};
+export default class App extends Component {
+  render() {
+    return (
+     <Main />
+    );
+  }
+}
+
+
